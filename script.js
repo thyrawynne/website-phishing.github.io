245f848aff9e17678b9ced8b1a68c178d3f2ad21
thyrawynne <thyra.wynne.mandak10@gmail.com>
2024-11-30 14:43:24 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get a reference to the button element
+const button = document.getElementById("myButton");
+
+// Add an event listener to the button
+button.addEventListener("click", () => {
+  // Change the background color of the body
+  document.body.style.backgroundColor = "lightblue";
+});
