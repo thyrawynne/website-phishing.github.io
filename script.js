69851afed1a66701662c9a5a7d0a2f5a50ab075c
thyrawynne <thyra.wynne.mandak10@gmail.com>
2024-12-01 22:27:14 +0800

    Javascript added

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-// Get a reference to the button element
-const button = document.getElementById("myButton");
-
-// Add an event listener to the button
-button.addEventListener("click", () => {
-  // Change the background color of the body
-  document.body.style.backgroundColor = "lightblue";
+fetch('https://<website-phishing-detection>.up.railway.app/predict/', {
+  method: 'POST',
+  headers: {
+      'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({ features: [/* fitur data */] }),
+})
+.then(response => response.json())
+.then(data => {
+  console.log('Prediction:', data.prediction);
+})
+.catch(error => {
+  console.error('Error:', error);
 });
